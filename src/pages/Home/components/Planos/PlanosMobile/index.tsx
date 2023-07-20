dd5688fb493561b7e5157eeec63723eba9088f0d
Gabriel Campos <52352805+gabrielcampoos@users.noreply.github.com>
2023-07-20 09:01:46 -0300

    Ajuste sessão de planos

--- src/pages/Home/components/Planos/PlanosMobile/index.tsx
+++ src/pages/Home/components/Planos/PlanosMobile/index.tsx
@@ -31,37 +31,38 @@ const PlanosMobile = () => {
 			component="main"
 			id="PlanosMobile"
 			width="100%"
-			height="70vh"
+			height="100%"
 			sx={{
 				backgroundColor: '#000',
+				padding: '3rem 0',
 			}}
 		>
 			<Container
+				component="main"
 				sx={{
 					width: '100%',
 					height: '100%',
 					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					justifyContent: 'center',
 				}}
 			>
 				<Box
 					width="100%"
-					height="70%"
+					height="100%"
 					display="flex"
 					flexDirection="column"
-					alignItems="center"
 					justifyContent="center"
+					// gap={5}
+					paddingTop="2rem "
 				>
 					<Section transition="all 2s 0.6s">
 						<Typography
 							component="h1"
+							variant='h2'
 							color="#fff"
 							fontSize={70}
-							lineHeight="4rem"
+							lineHeight="3.5rem"
 							fontFamily="Khand, sans-serif"
-							fontWeight={900}
+							fontWeight={700}
 							zIndex="1"
 						>
 							{' '}
@@ -69,14 +70,16 @@ const PlanosMobile = () => {
 						</Typography>
 					</Section>
 
-					<Section transition="all 1s 0.6s">
+					<Section transition="all 1s 0.6s"
+					>
 						<Typography
 							component="h1"
+							variant='h2'
 							color="#fff"
 							fontSize={70}
-							lineHeight="4rem"
+							lineHeight="3.5rem"
 							fontFamily="Khand, sans-serif"
-							fontWeight={900}
+							fontWeight={700}
 							zIndex="1"
 						>
 							NOSSOS <br />
@@ -85,46 +88,57 @@ const PlanosMobile = () => {
 					<Section transition="all 0.7s 0.6s">
 						<Typography
 							component="h1"
+							variant='h2'
 							color="#fff"
 							fontSize={70}
-							lineHeight="4rem"
+							lineHeight="3.5rem"
 							fontFamily="Khand, sans-serif"
-							fontWeight={900}
+							fontWeight={700}
 							zIndex="1"
+							marginBottom='3rem'
+
 						>
 							<span style={{ color: '#f6b332' }}>PLANOS</span>
 						</Typography>
+						<Typography component="p" color={'whitesmoke'} marginBottom='3rem'>
+							Ser atendido por ordem de chegada gera filas, isso
+							se tornou algo do passado. Use o Booksy para agendar
+							seus horários a qualquer hora, onde quer que esteja
+							e assim continue aproveitando da melhor maneira os
+							serviços que você adora.
+						</Typography>
 					</Section>
-				</Box>
-				<Box
-					width="100%"
-					height="30%"
-					display="flex"
-					flexDirection="column"
-					alignItems="center"
-					justifyContent="center"
-				>
 					<Section transition="all 2s 0.6s">
-						<Button
+					<Box
+					component="button"
+					width="100%"
+					height="11%"
+					border="none"
+					borderRadius="0.5rem"
+					sx={{backgroundColor: '#f6b332'}}
+					
+					
+				>
+					<a
 							href="https://api.whatsapp.com/send/?phone=5518997984039&text=Olá, gostaria de saber mais sobre os planos.&type=phone_number&app_absent=0"
-							variant="contained"
-							sx={{
-								backgroundColor: '#f6b332',
-								color: '#000',
-								width: '100%',
-								height: '70%',
-								border: 'none',
-								borderRadius: '0.5rem',
-								fontFamily: 'Khand, sans-serif',
-								fontWeight: 700,
-								fontSize: '2.3rem',
-								letterSpacing: '0.1rem',
+							style={{
+								textDecoration: 'none',
 							}}
 						>
-							SAIBA MAIS
-						</Button>
+						<Typography
+								color={'HighlightText'}
+								fontFamily="Khand, sans-serif"
+								fontWeight={700}
+								fontSize="2.3rem"
+								letterSpacing="0.1rem"
+							>
+								SAIBA MAIS
+							</Typography>
+						</a>
+				</Box>
 					</Section>
 				</Box>
+				
 			</Container>
 		</Box>
 	);
