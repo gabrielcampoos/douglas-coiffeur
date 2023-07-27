d3cfbd7464edcd1e80a097c42d8e6ce2afbd87a3
Gabriel Campos <gabrielcampoos@hotmail.com>
2023-07-27 00:25:43 -0300

    ultimos pedidos

--- src/pages/Home/components/Card/CardMobile/index.tsx
+++ src/pages/Home/components/Card/CardMobile/index.tsx
@@ -247,6 +247,119 @@ export default function ActionAreaCard() {
 				</Container>
 			</Card>
 
+			{/* MEDIUM */}
+
+			<Card
+				sx={{
+					width: '100%',
+					backgroundColor: '#f6b332',
+					padding: '0.2rem 0.2rem',
+				}}
+			>
+				<Container
+					disableGutters
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						border: '0.15rem solid #000',
+					}}
+				>
+					<CardActionArea>
+						<CardContent>
+							<Typography
+								gutterBottom
+								color={'HighlightText'}
+								fontFamily="Khand, sans-serif"
+								fontWeight={700}
+								fontSize="2.3rem"
+								letterSpacing="0.1rem"
+								textAlign="center"
+							>
+								MEDIUM
+							</Typography>
+							<Typography
+								gutterBottom
+								variant="h4"
+								component="div"
+								textAlign="center"
+							>
+								6 SERVIÇOS
+							</Typography>
+							<Typography variant="body2" textAlign="center">
+								<span
+									style={{
+										color: 'HighlightText',
+										fontFamily: 'Khand, sans-serif',
+										fontWeight: '400',
+										fontSize: '1.5rem',
+										letterSpacing: '0.1rem',
+										textAlign: 'center',
+									}}
+								>
+									De 240,00 por
+									<br />
+								</span>
+								<span
+									style={{
+										color: 'HighlightText',
+										fontFamily: 'Khand, sans-serif',
+										fontWeight: '700',
+										fontSize: '2rem',
+										letterSpacing: '0.1rem',
+										textAlign: 'center',
+									}}
+								>
+									R$ 180,00 <br />
+									por mês
+								</span>
+							</Typography>
+						</CardContent>
+					</CardActionArea>
+					<ControlledRadioButtonsGroup />
+					<Box
+						component="button"
+						width="70%"
+						height="100%"
+						margin="1rem 0"
+						border="none"
+						borderRadius="0.5rem"
+						zIndex="999999999999"
+						position="relative"
+						sx={{
+							backgroundColor: '#000',
+						}}
+					>
+						<a
+							href="https://api.whatsapp.com/send/?phone=5518997984039&text=Olá, gostaria de saber mais sobre o plano MEDIUM.&type=phone_number&app_absent=0"
+							style={{ textDecoration: 'none' }}
+						>
+							<Box
+								width="95%"
+								height="90%"
+								border="0.15rem solid #f6b332"
+								borderRadius="0.5rem"
+								position="absolute"
+								top="0.2rem"
+								left="0.3rem"
+								sx={{
+									backgroundColor: 'transparent',
+								}}
+							></Box>
+							<Typography
+								color={'whitesmoke'}
+								fontFamily="Khand, sans-serif"
+								fontWeight={700}
+								fontSize="1.5rem"
+								letterSpacing="0.1rem"
+							>
+								ASSINAR
+							</Typography>
+						</a>
+					</Box>
+				</Container>
+			</Card>
+
 			{/* PRO */}
 
 			<Card
